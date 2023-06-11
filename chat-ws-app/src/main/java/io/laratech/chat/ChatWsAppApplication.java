5cf8988fb65ceb2295ea4f7dc55ff20e6a011019
Alaa Hajjaj <alaa.hajjaj@laratech.io>
2023-06-11 22:13:49 +0200

    chat application

---
+++ chat-ws-app/src/main/java/io/laratech/chat/ChatWsAppApplication.java
@@ -0,0 +1,13 @@
+package io.laratech.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWsAppApplication.class, args);
+	}
+
+}
